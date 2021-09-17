4ff85e735a1964d66913b9223f750334755a6b2b
Grego <gregovilardo@gmail.com>
2021-09-16 23:48:19 -0300

    add slackeala

--- index.js
+++ index.js
@@ -1,65 +1,74 @@
-const home = document.querySelector("#home")
-const about = document.querySelector("#about")
-const projects = document.querySelector("#projects")
-const contact = document.querySelector("#contact")
-
-url = "http://127.0.0.1:5501"
 const swup = new Swup();
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const projects = document.querySelector("#projects");
+const contact = document.querySelector("#contact");
 
+url = "http://127.0.0.1:5501";
 
-setInterval(() => {
+let myInterval = setInterval(() => {
     nav_color();
-    load_project()
-}, 100);
+    load_project_vv();
+    load_project_slackeala();
+}, 500);
 
-
-// setTimeout(()=>{
-//     clearInterval(myInterval)
-// }, 10000)
-
+// setTimeout(()=>{clearInterval(myInterval)}, 10000);
 
 function nav_color() {
-    if (window.location.href === `${url}/index.html`){
-        home.style.color = "#7eca9c"
-        about.style.color = "#ffff"
-        projects.style.color = "#ffff"
+    if (window.location.href === `${url}/index.html`) {
+        home.style.color = "#7eca9c";
+        about.style.color = "#ffff";
+        projects.style.color = "#ffff";
     } else if (window.location.href === `${url}/about.html`) {
-        about.style.color = "#7eca9c"
-        home.style.color = "#ffff"
-        projects.style.color = "#ffff"
+        about.style.color = "#7eca9c";
+        home.style.color = "#ffff";
+        projects.style.color = "#ffff";
     } else if (window.location.href === `${url}/projects.html`) {
-        projects.style.color = "#7eca9c"
-        about.style.color = "#ffff"
-        home.style.color = "#ffff"
+        projects.style.color = "#7eca9c";
+        about.style.color = "#ffff";
+        home.style.color = "#ffff";
     }
 }
 
-function load_project(){
-
-    let link_project = document.querySelector("#link-pr")
+function load_project_vv() {
+    let link_project = document.querySelector("#link-pr-vv");
     if (link_project) {
-        link_project.onclick = () => { location.href='/vidriosvilardo.html' }
+        link_project.onclick = () => {
+            location.href = "/vidriosvilardo.html";
+        };
         link_project.addEventListener("keyup", (e) => {
             if (e.keyCode === 13) {
-                return location.href='/vidriosvilardo.html'
+                return (location.href = "/vidriosvilardo.html");
             }
-        })
+        });
     }
 }
 
-
+function load_project_slackeala() {
+    let link_project = document.querySelector("#link-pr-slackeala");
+    if (link_project) {
+        link_project.onclick = () => {
+            location.href = "/slackeala.html";
+        };
+        link_project.addEventListener("keyup", (e) => {
+            if (e.keyCode === 13) {
+                return (location.href = "/slackeala.html");
+            }
+        });
+    }
+}
 
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
     //Toggle Nav
-    
+
     burger.addEventListener("click", () => {
         nav.classList.toggle("nav-a");
-        setTimeout(()=> {
+        setTimeout(() => {
             nav.classList.toggle("nav-active");
-    
+
             //Animate Links
             navLinks.forEach((link, index) => {
                 if (link.style.animation) {
@@ -72,7 +81,7 @@ const navSlide = () => {
             });
             //burger animation
             burger.classList.toggle("toggle");
-        }, 50)
+        }, 50);
     });
 };
 
